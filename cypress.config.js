e3ee0ed97c83ed31f76464f26d729fba2ebbc15c
PrissVega <priscila@blubear.io>
2024-08-13 08:51:04 -0500

    Remove report function to isolate it from the main code and optimize api and DB data comparison to avoid duplicating code.

--- cypress.config.js
+++ cypress.config.js
@@ -1,19 +1,64 @@
 const { defineConfig } = require('cypress');
 const fs = require('fs');
 const path = require('path');
+const mysql = require('mysql2');
+
+let connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'HBstXZYxgIJJuzX7baCSdwnEI3',
+    database: 'postvetas_centra_v3'
+  });
+
+  connection.connect((err) => {
+    if (err) {
+      console.error('Error connecting: ' + err.stack);
+      return;
+    }
+    console.log('Connected as id ' + connection.threadId);
+  });
+
+  connection.on('error', (err) => {
+    console.error('DB error', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      // Reconnect if connection is lost
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
 
 module.exports = defineConfig({
   projectId: 'zimznw',
   e2e: {
     setupNodeEvents(on, config) {
-      // Registra la tarea personalizada 'writeFile'
+      // Registra la tarea personalizada 'queryDatabase'
       on('task', {
+        queryDatabase(query) {
+          return new Promise((resolve, reject) => {
+            if (!connection || connection.state === 'disconnected') {
+              handleDisconnect();
+            }
+            connection.query(query, (error, results) => {
+              if (error) {
+                return reject(error);
+              }
+              resolve(results);
+            });
+          });
+        },
+        // Registra la tarea personalizada 'writeFile'
         writeFile({ filePath, content }) {
           fs.writeFileSync(path.resolve(filePath), content, 'utf8');
           return null;
         }
       });
-      // Puedes registrar otras tareas aquí, si es necesario
     },
   }
 });
--- cypress/e2e/Endpoints_postventa.cy.js
+++ cypress/e2e/Endpoints_postventa.cy.js
@@ -1,6 +1,3 @@
-const fs = require('fs');
-const path = 'cypress/results';
-
 const params = {
   headers: {
     Authorization: 'Bearer 215|aMUuQFQaJ14Xar9eU2k55VChiyDgwRsqKos9SshU',
@@ -17,133 +14,103 @@ const normalizeData = (data, defaultValues = {}) => {
 
 // Función para obtener tipos de datos
 const getTypes = (obj) => {
-  return Object.fromEntries(Object.entries(obj).map(([key, value]) => [key, typeof value]));
+  const types = Object.fromEntries(Object.entries(obj).map(([key, value]) => [key, typeof value]));
+  delete types.links; // Excluir la propiedad 'links'
+  return types;
 };
 
-// Función para transformar tipos de datos
-const transformApiTypes = (apiTypes) => ({
-  id: Number(apiTypes.id),
-  instancia_id: Number(apiTypes.instancia_id),
-  empresa_id: Number(apiTypes.empresa_id),
-  agencias: String(apiTypes.agencias),
-  auto_id: Number(apiTypes.auto_id),
-  gestion_prin_id: String(apiTypes.gestion_prin_id),
-  gestion_general_tipo: String(apiTypes.gestion_general_tipo),
-  gestion_estado: String(apiTypes.gestion_estado),
-  users_id: Number(apiTypes.users_id),
-  usuarios3s_id: Number(apiTypes.usuarios3s_id),
-  usersugars_id: String(apiTypes.usersugars_id),
-  ordenes: Number(apiTypes.ordenes),
-  oportunidades: Number(apiTypes.oportunidades),
-  repuestos: Number(apiTypes.repuestos),
-  accesorios: Number(apiTypes.accesorios),
-  servicios: Number(apiTypes.servicios),
-  stock: Number(apiTypes.stock),
-  total_inicial: Number(apiTypes.total_inicial),
-  subtotal_inicialrepuestos: Number(apiTypes.subtotal_inicialrepuestos),
-  subtotal_inicialservicios: Number(apiTypes.subtotal_inicialservicios),
-  subtotal_inicialaccesorios: Number(apiTypes.subtotal_inicialaccesorios),
-  recuperado: Number(apiTypes.recuperado),
-  rechazado: Number(apiTypes.rechazado),
-  pendiente: Number(apiTypes.pendiente),
-  cita_fecha: apiTypes.cita_fecha,
-  llamada_fecha: apiTypes.llamada_fecha
-});
+describe('API Test SugarCRM/Gestion', () => {
+  const compareApiAndDbData = (apiData, dbQuery) => {
+    if (apiData && apiData.length > 0) {
+      const normalizedApiData = normalizeData(apiData, {
+        id: null,
+        instancia_id: null,
+        empresa_id: null,
+        agencias: null,
+        auto_id: null,
+        gestion_prin_id: null,
+        gestion_general_tipo: null,
+        gestion_estado: null,
+        users_id: null,
+        usuarios3s_id: null,
+        usersugars_id: null,
+        ordenes: null,
+        oportunidades: null,
+        repuestos: null,
+        accesorios: null,
+        servicios: null,
+        stock: null,
+        total_inicial: null,
+        subtotal_inicialrepuestos: null,
+        subtotal_inicialservicios: null,
+        subtotal_inicialaccesorios: null,
+        recuperado: null,
+        rechazado: null,
+        pendiente: null,
+        cita_fecha: null,
+        llamada_fecha: null
+      });
 
-describe('API Test', () => {
+      cy.task('queryDatabase', dbQuery)
+        .then(datosDB => {
+          const normalizedDbData = normalizeData(datosDB, {
+            id: null,
+            instancia_id: null,
+            empresa_id: null,
+            agencias: null,
+            auto_id: null,
+            gestion_prin_id: null,
+            gestion_general_tipo: null,
+            gestion_estado: null,
+            users_id: null,
+            usuarios3s_id: null,
+            usersugars_id: null,
+            ordenes: null,
+            oportunidades: null,
+            repuestos: null,
+            accesorios: null,
+            servicios: null,
+            stock: null,
+            total_inicial: null,
+            subtotal_inicialrepuestos: null,
+            subtotal_inicialservicios: null,
+            subtotal_inicialaccesorios: null,
+            recuperado: null,
+            rechazado: null,
+            pendiente: null,
+            cita_fecha: null,
+            llamada_fecha: null
+          });
+
+          const apiTypes = getTypes(normalizedApiData[0]);
+          const dbTypes = getTypes(normalizedDbData[0]);
+
+          expect(JSON.stringify(apiTypes)).to.equal(JSON.stringify(dbTypes));
+        });
+    } else {
+      cy.log('No se encontraron datos en la respuesta de la API.');
+    }
+  };
+
   it('should return a successful response from the API and compare types with DB data: SugarCRM_Gestion_gestion_all', () => {
     cy.request({
-      url:'https://api-sugarcrm.casabaca.loc/api/v2/postventa/sugar_ordencabecera/1?appId=c81e728d9d4c2f636f067f89cc14862c&usuId=2',
+      url:'https://api-sugarcrm.casabaca.loc/api/v2/postventa/sugar_gestion?appId=c81e728d9d4c2f636f067f89cc14862c&usuId=2',
       method: 'GET',
       headers: params.headers,
       failOnStatusCode: false
     }).then(response => {
-      const apiData = response.body.data;
-      const hasData = apiData && apiData.length > 0;
-      const result = {
-        status: response.status,
-        hasData,
-        apiData
-      };
+      compareApiAndDbData(response.body.data, 'SELECT PG.id,PG.instancia_id,POC.empresa_id,POC.codAgencia agencias,PG.auto_id,PG.gestion_prin_id,PG.gestion_general_tipo,PG.gestion_estado,PG.users_id,PG.usuarios3s_id,PG.usersugars_id,PG.ordenes,PG.oportunidades,PG.repuestos,PG.accesorios,PG.servicios,PG.stock,PG.total_inicial,PG.subtotal_inicialrepuestos,PG.subtotal_inicialservicios,PG.subtotal_inicialaccesorios,PG.recuperado,PG.rechazado,PG.pendiente,PG.cita_fecha,PG.llamada_fecha FROM postvetas_centra_v3.pvt_gestions PG inner join postvetas_centra_v3.pvt_orden_cabeceras POC on PG.auto_id = POC.auto_id;');
+    });
+  });
 
-      if (hasData) {
-        const normalizedApiData = normalizeData(apiData, {
-          id: null,
-          instancia_id: null,
-          empresa_id: null,
-          agencias: null,
-          auto_id: null,
-          gestion_prin_id: null,
-          gestion_general_tipo: null,
-          gestion_estado: null,
-          users_id: null,
-          usuarios3s_id: null,
-          usersugars_id: null,
-          ordenes: null,
-          oportunidades: null,
-          repuestos: null,
-          accesorios: null,
-          servicios: null,
-          stock: null,
-          total_inicial: null,
-          subtotal_inicialrepuestos: null,
-          subtotal_inicialservicios: null,
-          subtotal_inicialaccesorios: null,
-          recuperado: null,
-          rechazado: null,
-          pendiente: null,
-          cita_fecha: null,
-          llamada_fecha: null
-        });
-
-        cy.task('queryDatabase', 'SELECT ...')
-          .then(datosDB => {
-            const normalizedDbData = normalizeData(datosDB, {
-              id: null,
-              instancia_id: null,
-              empresa_id: null,
-              agencias: null,
-              auto_id: null,
-              gestion_prin_id: null,
-              gestion_general_tipo: null,
-              gestion_estado: null,
-              users_id: null,
-              usuarios3s_id: null,
-              usersugars_id: null,
-              ordenes: null,
-              oportunidades: null,
-              repuestos: null,
-              accesorios: null,
-              servicios: null,
-              stock: null,
-              total_inicial: null,
-              subtotal_inicialrepuestos: null,
-              subtotal_inicialservicios: null,
-              subtotal_inicialaccesorios: null,
-              recuperado: null,
-              rechazado: null,
-              pendiente: null,
-              cita_fecha: null,
-              llamada_fecha: null
-            });
-
-            const apiTypes = getTypes(normalizedApiData[0]);
-            const dbTypes = getTypes(normalizedDbData[0]);
-
-            result.apiTypes = apiTypes;
-            result.dbTypes = dbTypes;
-            result.comparison = {
-              apiTypesMatch: JSON.stringify(apiTypes) === JSON.stringify(dbTypes)
-            };
-
-            cy.task('writeFile', { filePath: 'cypress/results/report.json', content: JSON.stringify(result, null, 2) });
-
-            expect(result.comparison.apiTypesMatch).to.be.true;
-          });
-      } else {
-        cy.task('writeFile', { filePath: 'cypress/results/report.json', content: JSON.stringify(result, null, 2) });
-        cy.log('No se encontraron datos en la respuesta de la API.');
-      }
+  it('should return a successful response from the API and compare types with DB data: SugarCRM_Gestion_gestion_individual', () => {
+    cy.request({
+      url:'https://api-sugarcrm.casabaca.loc/api/v2/postventa/sugar_gestion/4?appId=c81e728d9d4c2f636f067f89cc14862c&usuId=2',
+      method: 'GET',
+      headers: params.headers,
+      failOnStatusCode: false
+    }).then(response => {
+      compareApiAndDbData(response.body.data, 'SELECT PG.id,PG.instancia_id,POC.empresa_id,POC.codAgencia agencias,PG.auto_id,PG.gestion_prin_id,PG.gestion_general_tipo,PG.gestion_estado,PG.users_id,PG.usuarios3s_id,PG.usersugars_id,PG.ordenes,PG.oportunidades,PG.repuestos,PG.accesorios,PG.servicios,PG.stock,PG.total_inicial,PG.subtotal_inicialrepuestos,PG.subtotal_inicialservicios,PG.subtotal_inicialaccesorios,PG.recuperado,PG.rechazado,PG.pendiente,PG.cita_fecha,PG.llamada_fecha FROM postvetas_centra_v3.pvt_gestions PG inner join postvetas_centra_v3.pvt_orden_cabeceras POC on PG.auto_id = POC.auto_id;');
     });
   });
 });
