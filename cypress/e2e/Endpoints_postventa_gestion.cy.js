061ad14ff834b661e8f27f27beafc23e4d5b0f1d
PrissVega <priscila@blubear.io>
2024-08-13 09:57:57 -0500

    complete API test: Management
    se completa las pruebas de respuesta, comparación de data y tipo de dato entre BD y API del módulo de gestión

--- cypress/e2e/Endpoints_postventa_gestion.cy.js
+++ cypress/e2e/Endpoints_postventa_gestion.cy.js
@@ -1,3 +1,4 @@
+// Define params al inicio del archivo
 const params = {
   headers: {
     Authorization: 'Bearer 215|aMUuQFQaJ14Xar9eU2k55VChiyDgwRsqKos9SshU',
@@ -12,10 +13,18 @@ const normalizeData = (data, defaultValues = {}) => {
     [ { ...defaultValues, ...data } ];
 };
 
+const normalizeDataOrdenCabecera = (data, defaultValues = {}) => {
+  return Array.isArray(data) ? 
+    data.map(item => ({ ...defaultValues, ...item })) :
+    [ { ...defaultValues, ...data } ];
+};
+
 // Función para obtener tipos de datos
 const getTypes = (obj) => {
   const types = Object.fromEntries(Object.entries(obj).map(([key, value]) => [key, typeof value]));
   delete types.links; // Excluir la propiedad 'links'
+  delete types.gestion_prin_cabecera; // Excluir 'gestion_prin_cabecera'
+  delete types.agencias;
   return types;
 };
 
@@ -92,9 +101,52 @@ describe('API Test SugarCRM/Gestion', () => {
     }
   };
 
+  // Para orden Cabecera
+  const compareApiAndDbDataOrdenCabecera = (apiData, dbQuery) => {
+    if (apiData && apiData.length > 0) {
+      const normalizedApiDataOrdenCabecera = normalizeDataOrdenCabecera(apiData, {
+        id: null,
+        instancia_id: null,
+        empresa_id: null,
+        auto_id: null,
+        gestion_id: null,
+        ordTaller: null,
+        ordFechaCrea: null,
+        codOrdAsesor: null,
+        nomOrdAsesor: null,
+        codAgencia: null,
+        nomAgencia: null
+      });
+
+      cy.task('queryDatabase', dbQuery)
+        .then(datosDB => {
+          const normalizedDbDataOrdenCabecera = normalizeDataOrdenCabecera(datosDB, {
+            id: null,
+            instancia_id: null,
+            empresa_id: null,
+            auto_id: null,
+            gestion_id: null,
+            ordTaller: null,
+            ordFechaCrea: null,
+            codOrdAsesor: null,
+            nomOrdAsesor: null,
+            codAgencia: null,
+            nomAgencia: null
+          });
+
+          const apiTypes = getTypes(normalizedApiDataOrdenCabecera[0]);
+          const dbTypes = getTypes(normalizedDbDataOrdenCabecera[0]);
+
+          expect(JSON.stringify(apiTypes)).to.equal(JSON.stringify(dbTypes));
+        });
+    } else {
+      cy.log('No se encontraron datos en la respuesta de la API.');
+    }
+  };
+
   it('should return a successful response from the API and compare types with DB data: SugarCRM_Gestion_gestion_all', () => {
     cy.request({
-      url:'https://api-sugarcrm.casabaca.loc/api/v2/postventa/sugar_gestion?appId=c81e728d9d4c2f636f067f89cc14862c&usuId=2',
+      url: 'https://api-sugarcrm.casabaca.loc/api/v2/postventa/sugar_gestion?appId=c81e728d9d4c2f636f067f89cc14862c&usuId=2',
       method: 'GET',
       headers: params.headers,
       failOnStatusCode: false
@@ -105,12 +157,23 @@ describe('API Test SugarCRM/Gestion', () => {
 
   it('should return a successful response from the API and compare types with DB data: SugarCRM_Gestion_gestion_individual', () => {
     cy.request({
-      url:'https://api-sugarcrm.casabaca.loc/api/v2/postventa/sugar_gestion/4?appId=c81e728d9d4c2f636f067f89cc14862c&usuId=2',
+      url: 'https://api-sugarcrm.casabaca.loc/api/v2/postventa/sugar_gestion/4?appId=c81e728d9d4c2f636f067f89cc14862c&usuId=2',
       method: 'GET',
       headers: params.headers,
       failOnStatusCode: false
     }).then(response => {
       compareApiAndDbData(response.body.data, 'SELECT PG.id,PG.instancia_id,POC.empresa_id,POC.codAgencia agencias,PG.auto_id,PG.gestion_prin_id,PG.gestion_general_tipo,PG.gestion_estado,PG.users_id,PG.usuarios3s_id,PG.usersugars_id,PG.ordenes,PG.oportunidades,PG.repuestos,PG.accesorios,PG.servicios,PG.stock,PG.total_inicial,PG.subtotal_inicialrepuestos,PG.subtotal_inicialservicios,PG.subtotal_inicialaccesorios,PG.recuperado,PG.rechazado,PG.pendiente,PG.cita_fecha,PG.llamada_fecha FROM postvetas_centra_v3.pvt_gestions PG inner join postvetas_centra_v3.pvt_orden_cabeceras POC on PG.auto_id = POC.auto_id;');
+    });
+  });
+
+  it('should return a successful response from the API and compare types with DB data: SugarCRM_Gestion_orden_cabecera', () => {
+    cy.request({
+      url: 'https://api-sugarcrm.casabaca.loc/api/v2/postventa/sugar_gestion/1/ordenCabecera?appId=c81e728d9d4c2f636f067f89cc14862c&usuId=2',
+      method: 'GET',
+      headers: params.headers,
+      failOnStatusCode: false
+    }).then(response => {
+      compareApiAndDbDataOrdenCabecera(response.body.data, 'SELECT PG.id, PG.instancia_id, PG.empresa_id, PG.auto_id, POC.gestion_id, POC.ordTaller, POC.ordFechaCrea, POC.codOrdAsesor, POC.nomOrdAsesor, POC.codAgencia, POC.nomAgencia FROM postvetas_centra_v3.pvt_gestions PG inner join postvetas_centra_v3.pvt_orden_cabeceras POC on PG.id = POC.id and PG.instancia_id = POC.instancia_id and PG.empresa_id = POC.empresa_id');
     });
   });
 });
